4e063f3cd737c88b70365167af6f33ffd95e13b0
chidieb11 <chukwuchidieberejohn@gmail.com>
2022-04-10 15:21:49 +0100

    Initial commit

--- index.js
+++ index.js
@@ -30,13 +30,6 @@ app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
 app.use("/api/checkout", stripeRoute);
 
-// Needed during deployment
-// app.use(express.static(path.join(__dirname, "/app/build")));
-
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "/app/build", "index.html"));
-// });
-
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`BACKEND IS RUNNING ON PORT ${port}`);
